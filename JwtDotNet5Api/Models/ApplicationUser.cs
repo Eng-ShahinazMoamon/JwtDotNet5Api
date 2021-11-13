43ea9d2deae9ec76c16f621309940910b186fb24
shahinazmoamen <shahinazmoamen@gmail.com>
2021-11-13 17:15:04 +0200

    up

---
+++ JwtDotNet5Api/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtDotNet5Api.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+
+        [Required, MaxLength(50)]
+        public string firstName { get; set; }
+
+        [Required, MaxLength(50)]
+        public string lastName { get; set; }
+    }
+}
--- JwtDotNet5Api/Startup.cs
+++ JwtDotNet5Api/Startup.cs
@@ -1,7 +1,11 @@
+using JwtDotNet5Api.Helper;
+using JwtDotNet5Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,12 +30,19 @@ namespace JwtDotNet5Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Configure system
+            services.Configure<JWT>(Configuration.GetSection("JWT"));
+            //To explian to system you are using Identity 
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            //Connection String 
+            services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JwtDotNet5Api", Version = "v1" });
             });
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
