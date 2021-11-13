37570bf940065eb3364fddd395016c001e217237
shahinazmoamen <shahinazmoamen@gmail.com>
2021-11-13 17:56:29 +0200

    up

---
+++ JwtDotNet5Api/Migrations/20211113153234_seeding.cs
@@ -0,0 +1,29 @@
+﻿using JwtDotNet5Api.Helper;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace JwtDotNet5Api.Migrations
+{
+    public partial class seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns:new[] {"Id","Name", "NormalizedName", "ConcurrencyStamp" },
+                values:new object [] {Guid.NewGuid().ToString(),"Admin", Roles.Admin.ToUpper(), Guid.NewGuid().ToString()}     
+                );
+            migrationBuilder.InsertData(
+           table: "AspNetRoles",
+           columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+           values: new object[] { Guid.NewGuid().ToString(), "User", Roles.User.ToUpper(), Guid.NewGuid().ToString() }
+           );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete From [AspNetRoles]");
+        }
+    }
+}
